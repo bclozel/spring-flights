d97a6bba6404b29d7566ebfea1f88d1191d4ce03
Rob Winch <rwinch@users.noreply.github.com>
2019-09-25 14:35:09 -0500

    Add fetchProfileMe test

--- flight-tracker/src/test/java/io/spring/sample/flighttracker/FlightTrackerApplicationTests.java
+++ flight-tracker/src/test/java/io/spring/sample/flighttracker/FlightTrackerApplicationTests.java
@@ -1,16 +1,51 @@
 package io.spring.sample.flighttracker;
 
+import io.spring.sample.flighttracker.profile.UserProfile;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FlightTrackerApplicationTests {
 
+	@Autowired
+	private RSocketRequester.Builder requesterBuilder;
+
+	private URI uri;
+
 	@Test
-	public void contextLoads() {
+	public void fetchProfileMe() {
+		String login = "rossen";
+		Mono<RSocketRequester> requester = this.requesterBuilder
+				.dataMimeType(MediaType.APPLICATION_CBOR)
+				.connectWebSocket(this.uri);
+
+		Mono<UserProfile> profile = requester.flatMap(req ->
+				req.route("fetch.profile.me")
+					.retrieveMono(UserProfile.class)
+		);
+
+		StepVerifier.create(profile)
+				.assertNext(userProfile -> {
+					assertThat(userProfile.getLogin()).isEqualTo(login);
+				})
+				.verifyComplete();
 	}
 
+	@LocalServerPort
+	public void setPort(int port) {
+		this.uri = URI.create("ws://localhost:" + port + "/rsocket");
+	}
 }
