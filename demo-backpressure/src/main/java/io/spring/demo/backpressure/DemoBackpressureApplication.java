fbf337903387d5799ae37967bcbb721d3947f9b8
Brian Clozel <bclozel@pivotal.io>
2019-10-02 14:45:53 +0200

    Add demo-backpressure application

---
+++ demo-backpressure/src/main/java/io/spring/demo/backpressure/DemoBackpressureApplication.java
@@ -0,0 +1,13 @@
+package io.spring.demo.backpressure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackpressureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackpressureApplication.class, args);
+	}
+
+}
