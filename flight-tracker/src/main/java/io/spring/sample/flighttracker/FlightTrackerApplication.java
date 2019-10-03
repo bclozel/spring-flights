c303ed019712528cd17c5fe9ce1ac544015cd9eb
Brian Clozel <bclozel@pivotal.io>
2019-10-03 15:48:39 +0200

    Create Flight Tracker application
    This application will serve static resources from the "flight-client"
    application as well as RSocket and HTTP requests sent by the browser.
    
    Because we'll store user profile information, we'll also need Spring
    Data MongoDB support.

---
+++ flight-tracker/src/main/java/io/spring/sample/flighttracker/FlightTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.spring.sample.flighttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTrackerApplication.class, args);
+	}
+
+}
