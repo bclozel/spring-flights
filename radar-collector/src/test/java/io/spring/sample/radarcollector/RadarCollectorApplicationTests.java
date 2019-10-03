1e250cbc26a28a66f08bdc37d5bca71438351be9
Brian Clozel <bclozel@pivotal.io>
2019-10-03 14:43:37 +0200

    Configure RSocket server and add first mapping
    First, we need to configure our RSocket server; for that, we select TCP
    as our RSocket transport and chose a TCP port our standalone RSocket
    will be listening on.
    
    We can then create our first controller and `@MessageMapping` that lets
    us find an airport radar given its airport code, `"find.radar.{code}"`.
    
    We also have a `@SpringBootTest` annotated class that checks this use
    case.

--- radar-collector/src/test/java/io/spring/sample/radarcollector/RadarCollectorApplicationTests.java
+++ radar-collector/src/test/java/io/spring/sample/radarcollector/RadarCollectorApplicationTests.java
@@ -1,14 +1,42 @@
 package io.spring.sample.radarcollector;
 
+import io.spring.sample.radarcollector.radars.AirportLocation;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.rsocket.context.LocalRSocketServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(properties = "spring.rsocket.server.port=0")
 class RadarCollectorApplicationTests {
 
+	private static Logger logger = LoggerFactory.getLogger(RadarCollectorApplicationTests.class);
+
 	@Test
-	void contextLoads() {
+	void findRadarByCode(@Autowired RSocketRequester.Builder requesterBuilder, @LocalRSocketServerPort int port) {
+		Mono<RSocketRequester> requester = requesterBuilder
+				.dataMimeType(MediaType.APPLICATION_CBOR)
+				.connectTcp("localhost", port);
+
+		Mono<AirportLocation> airport = requester.flatMap(req ->
+				req.route("find.radar.{code}", "LYS")
+						.retrieveMono(AirportLocation.class));
+
+		StepVerifier.create(airport)
+				.assertNext(airportLocation -> {
+					logger.info("Airport found for code 'LYS': {}", airportLocation);
+					assertThat(airportLocation.getCode()).isEqualTo("LYS");
+					assertThat(airportLocation.getName()).isEqualTo("Lyon Saint-Exupery Airport");
+				})
+				.verifyComplete();
 	}
 
 }
