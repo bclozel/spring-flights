62be895305ff22ec1639c15c0683f35cf832a135
Brian Clozel <bclozel@pivotal.io>
2019-10-03 15:18:44 +0200

    Add Flight client application
    The Flight Client module is a JavaScript client application that
    renders a Leaflet map on the browser and allows user interactions with
    that map.
    The client is using the RSocket WebSocket client from rsocket-js to
    communicate with the RSocket server of the Flight Tracker app.

---
+++ flight-client/src/index.js
@@ -0,0 +1,10 @@
+import 'bulma/css/bulma.min.css';
+import {RadarMap} from './map';
+
+const zoomLevel = 10;
+
+window.onload = function init() {
+    const map = new RadarMap(zoomLevel);
+};
+
+
---
+++ flight-client/src/metadata.js
@@ -0,0 +1,47 @@
+import {
+    JsonSerializer
+} from 'rsocket-core';
+
+export class Metadata extends Map {
+
+    constructor(json) {
+        super();
+        if (json != null) {
+            for (let [key, value] of Object.entries(json)) {
+                this.set(key, value);
+            }
+        }
+    }
+
+    toJSON() {
+        const result = {};
+        for (let [key, value] of this.entries()) {
+            result[key] = value;
+        }
+        return result;
+    }
+
+}
+Metadata.ROUTE = "route";
+Metadata.AUTHENTICATION_BEARER = "message/x.rsocket.authentication.bearer.v0";
+
+export const JsonMetadataSerializer = {
+
+    deserialize(data) {
+        if (data == null) {
+            return null;
+        }
+        let json = JsonSerializer.deserialize(data);
+        return new Metadata(json);
+    },
+
+    serialize(metadata) {
+        if (metadata == null) {
+            return null;
+        }
+        let json = metadata.toJSON();
+        return JsonSerializer.serialize(json);
+    }
+
+};
+JsonMetadataSerializer.MIME_TYPE = "application/vnd.spring.rsocket.metadata+json";
