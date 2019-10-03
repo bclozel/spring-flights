a542207261af83f3113feb479765ad963392f12b
Brian Clozel <bclozel@pivotal.io>
2019-10-03 16:06:50 +0200

    Add radar endpoints
    This commit creates a `RadarService` component that connects to the
    radar-collector application and is able to send RSocket requests to it.
    
    We can then expose new RSocket endpoints in this application and fetch
    data about specific airports, fetch airports within a particular area
    and zip multiple streams of aircrafts signals for several airports.

---
+++ flight-tracker/src/main/java/io/spring/sample/flighttracker/radars/RadarsController.java
@@ -0,0 +1,70 @@
+package io.spring.sample.flighttracker.radars;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RadarsController {
+
+	private final Logger logger = LoggerFactory.getLogger(RadarsController.class);
+
+	private final RadarService radarService;
+
+	private final Queue<RSocketRequester> connectedClients = new ConcurrentLinkedQueue<>();
+
+	public RadarsController(RadarService radarService) {
+		this.radarService = radarService;
+	}
+
+	@MessageMapping("locate.radar.{code}")
+	public Mono<AirportLocation> radar(@DestinationVariable String code) {
+		return this.radarService.findRadar(code);
+	}
+
+	@MessageMapping("locate.radars.within")
+	public Flux<AirportLocation> radars(MapRequest request) {
+		return this.radarService.findRadars(request.getViewBox(), request.getMaxRadars());
+	}
+
+	@MessageMapping("locate.aircrafts.for")
+	public Flux<AircraftSignal> streamAircraftSignal(List<Radar> radars, RSocketRequester requester) {
+		this.connectedClients.offer(requester);
+		return this.radarService.streamAircraftSignals(radars).onBackpressureDrop()
+				.doOnTerminate(() -> {
+					this.logger.info("Server error while streaming data to the client");
+					this.connectedClients.remove(requester);
+				})
+				.doOnCancel(() -> {
+					this.logger.info("Connection closed by the client");
+					this.connectedClients.remove(requester);
+				});
+	}
+
+	@PostMapping("/location/{code}")
+	@ResponseBody
+	public Mono<String> sendClientsToLocation(@PathVariable String code) {
+		return this.radarService.findRadar(code)
+				.flatMapMany(radar ->
+						Flux.fromIterable(this.connectedClients)
+								.flatMap(requester -> sendRadarLocation(requester, radar)))
+				.then(Mono.just("Sent clients to location " + code));
+	}
+
+	private Mono<Void> sendRadarLocation(RSocketRequester requester, AirportLocation radar) {
+		return requester.route("send.to.location").data(radar).send();
+	}
+}
