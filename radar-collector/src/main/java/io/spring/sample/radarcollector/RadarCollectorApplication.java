39d8799bee146d1ca164f330c26b9da723703f17
Brian Clozel <bclozel@pivotal.io>
2019-10-03 11:20:03 +0200

    Create Radar Collector application
    This commit adds a newly created Spring Boot application using
    start.spring.io
    
    In this application, we'll need:
    * Spring Data MongoDB reactive for storing airport data
    * RSocket for exposing endpoints
    * Spring Boot devtools to help during the development cycle

---
+++ radar-collector/src/main/java/io/spring/sample/radarcollector/RadarCollectorApplication.java
@@ -0,0 +1,13 @@
+package io.spring.sample.radarcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadarCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadarCollectorApplication.class, args);
+	}
+
+}
