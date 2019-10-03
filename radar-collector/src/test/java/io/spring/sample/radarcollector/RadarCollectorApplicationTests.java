1a681cabc0981e2289e44e85a7aa3ae22056e87f
Brian Clozel <bclozel@pivotal.io>
2019-10-03 11:20:03 +0200

    Create Radar Collector application
    This commit adds a newly created Spring Boot application using
    start.spring.io
    
    In this application, we'll need:
    * Spring Data MongoDB reactive for storing airport data
    * RSocket for exposing endpoints
    * Spring Boot devtools to help during the development cycle

---
+++ radar-collector/src/test/java/io/spring/sample/radarcollector/RadarCollectorApplicationTests.java
@@ -0,0 +1,14 @@
+package io.spring.sample.radarcollector;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadarCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
